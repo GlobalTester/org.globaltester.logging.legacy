8cffc3abcff9cd7a6cff4f00057879407446c40b
Jacob Goeke <jacob.goeke@hjp-consulting.com>
2016-01-08 14:51:06 +0100

    removed use of deprecated function
    changed default value of preference P_TEST_LOG_SINGLE_TESTCASES

--- org.globaltester.logging.legacy/src/org/globaltester/logging/preferences/PreferenceInitializer.java
+++ org.globaltester.logging.legacy/src/org/globaltester/logging/preferences/PreferenceInitializer.java
@@ -17,10 +17,8 @@ package org.globaltester.logging.preferences;
 
 import org.eclipse.core.runtime.Platform;
 import org.eclipse.core.runtime.preferences.AbstractPreferenceInitializer;
-import org.eclipse.core.runtime.preferences.DefaultScope;
+import org.eclipse.jface.preference.IPreferenceStore;
 import org.globaltester.logging.Activator;
-import org.osgi.service.prefs.BackingStoreException;
-import org.osgi.service.prefs.Preferences;
 
 /**
  * This class stores the properties of the plugin Class used to initialize
@@ -37,63 +35,55 @@ public class PreferenceInitializer extends AbstractPreferenceInitializer {
 	 * at PreferenceConstants.java
 	 */
 	public void initializeDefaultPreferences() {
-
-		Preferences preferences = new DefaultScope()
-				.getNode(Activator.PLUGIN_ID);
 		
-
+		IPreferenceStore store = Activator.getDefault()
+				.getPreferenceStore();
+		
 		// switch to set manual directory settings
-		preferences.putBoolean(PreferenceConstants.P_MANUALFRAMEWORKDIRSETTINGS, false);
-		preferences.putBoolean(PreferenceConstants.P_MANUALDIRSETTINGS, false);
-		preferences.putBoolean(PreferenceConstants.P_USE_SIM_LOG, false);
+		store.setDefault(PreferenceConstants.P_MANUALFRAMEWORKDIRSETTINGS, false);
+		store.setDefault(PreferenceConstants.P_MANUALDIRSETTINGS, false);
+		store.setDefault(PreferenceConstants.P_USE_SIM_LOG, false);
 
 		// directory name of logging files
-		preferences.put(PreferenceConstants.P_GT_LOGGINGDIR, Platform
+		store.setDefault(PreferenceConstants.P_GT_LOGGINGDIR, Platform
 				.getLocation().append(".metadata").toString());
-		preferences.put(PreferenceConstants.P_TEST_LOGGINGDIR,
+		store.setDefault(PreferenceConstants.P_TEST_LOGGINGDIR,
 				System.getProperty("user.home"));
-		preferences.put(PreferenceConstants.P_GT_SIM_LOGGINGDIR,
+		store.setDefault(PreferenceConstants.P_GT_SIM_LOGGINGDIR,
 				System.getProperty("user.home"));
 
 		// log level of log file
-		preferences.putInt(PreferenceConstants.P_GT_LOGLEVEL,
+		store.setDefault(PreferenceConstants.P_GT_LOGLEVEL,
 				PreferenceConstants.LOGLEVEL_TRACE);
-		preferences.putInt(PreferenceConstants.P_TEST_LOGLEVEL,
+		store.setDefault(PreferenceConstants.P_TEST_LOGLEVEL,
 				PreferenceConstants.LOGLEVEL_INFO);
-		preferences.putInt(PreferenceConstants.P_GT_SIM_LOGLEVEL,
+		store.setDefault(PreferenceConstants.P_GT_SIM_LOGLEVEL,
 				PreferenceConstants.LOGLEVEL_INFO);
 
 		// switch to use html log file
-		preferences.putBoolean(PreferenceConstants.P_GT_HTMLLOGGING, false);
-		preferences.putBoolean(PreferenceConstants.P_TEST_HTMLLOGGING, false);
-		preferences.putBoolean(PreferenceConstants.P_GT_SIM_HTMLLOGGING, false);
+		store.setDefault(PreferenceConstants.P_GT_HTMLLOGGING, false);
+		store.setDefault(PreferenceConstants.P_TEST_HTMLLOGGING, false);
+		store.setDefault(PreferenceConstants.P_GT_SIM_HTMLLOGGING, false);
 
 		// switch to use plain log file
-		preferences.putBoolean(PreferenceConstants.P_GT_PLAINLOGGING, true);
-		preferences.putBoolean(PreferenceConstants.P_TEST_PLAINLOGGING, true);
-		preferences.putBoolean(PreferenceConstants.P_GT_SIM_PLAINLOGGING, true);
-		preferences.putBoolean(PreferenceConstants.P_TEST_LOG_SINGLE_TESTCASES, true);
+		store.setDefault(PreferenceConstants.P_GT_PLAINLOGGING, true);
+		store.setDefault(PreferenceConstants.P_TEST_PLAINLOGGING, true);
+		store.setDefault(PreferenceConstants.P_GT_SIM_PLAINLOGGING, true);
+		store.setDefault(PreferenceConstants.P_TEST_LOG_SINGLE_TESTCASES, false);
 
 		// log in ISO8601 format
-		preferences.putBoolean(PreferenceConstants.P_GT_USEISO8601LOGGING, true);
-		preferences.putBoolean(PreferenceConstants.P_TEST_USEISO8601LOGGING, true);
-		preferences.putBoolean(PreferenceConstants.P_GT_SIM_USEISO8601LOGGING, true);
+		store.setDefault(PreferenceConstants.P_GT_USEISO8601LOGGING, true);
+		store.setDefault(PreferenceConstants.P_TEST_USEISO8601LOGGING, true);
+		store.setDefault(PreferenceConstants.P_GT_SIM_USEISO8601LOGGING, true);
 
 		// switch to use console logging
-		preferences.putBoolean(PreferenceConstants.P_GT_CONSOLELOGGING, true);
+		store.setDefault(PreferenceConstants.P_GT_CONSOLELOGGING, true);
 
 		// store all marker of log file persistent
-		preferences.putBoolean(PreferenceConstants.P_TEST_PERSISTENTMARKER, false);
+		store.setDefault(PreferenceConstants.P_TEST_PERSISTENTMARKER, false);
 		
 		//if TestCase or log file should open on double click
-		preferences.putInt(PreferenceConstants.P_DOUBLECLICKRESULTVIEW, 0);
-		
-		// Force the application to save the preferences
-		try {
-			preferences.flush();
-		} catch (BackingStoreException e) {
-			e.printStackTrace();
-		}
+		store.setDefault(PreferenceConstants.P_DOUBLECLICKRESULTVIEW, 0);
 
 	}
 
